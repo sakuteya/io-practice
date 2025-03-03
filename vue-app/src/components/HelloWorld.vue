7261e712e6c193b6dd01a3f382418ea02d5cb3a1
dearka_vermilion <dearka.vermilion@gmail.com>
2025-03-03 16:28:25 +0900

    test

--- vue-app/src/components/HelloWorld.vue
+++ vue-app/src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>hoge</p>
+    <p>feature/test</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
