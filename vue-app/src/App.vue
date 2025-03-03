6dc8c5b4761c48b81738879e4088acea3e6b0d3a
dearka_vermilion <dearka.vermilion@gmail.com>
2025-03-03 16:05:18 +0900

    eslint.yml作成

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld2 from './components/HelloWorld2.vue'
 
 export default {
   name: 'App',
