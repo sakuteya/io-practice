46ba8afdeef95936e623eb92c6eab1ce6e452427
dearka_vermilion <dearka.vermilion@gmail.com>
2025-03-04 10:31:58 +0900

    自動ビルドテスト

--- vue-app/src/components/HelloWorld.vue
+++ vue-app/src/components/HelloWorld.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>hoge</p>
-    <p>test2</p>
+    <p>自動ビルドテスト</p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
