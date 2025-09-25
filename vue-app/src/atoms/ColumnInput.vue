e290796f8c694d282d89b5ac68b391cb1b9cc84c
sakuteya <dearka.vermilion@gmail.com>
2025-09-25 22:20:37 +0900

    eslintがいうこと聞かない

---
+++ vue-app/src/atoms/ColumnInput.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+const model = defineModel()
+
+</script>
+
+<template>
+  <div class="columns">
+    <div class="column is-half">
+      <!-- {{ label }} -->
+      入力欄3
+    </div>
+    <div class="column">
+      <input type="text" class="input" v-model="model" />
+    </div>
+  </div>
+</template>
--- vue-app/src/components/HogeFuga.vue
+++ vue-app/src/components/HogeFuga.vue
@@ -1,5 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+import CountButton from '../atoms/CountButton.vue'
+import ColumnInput from '../atoms/ColumnInput.vue'
+
+// defineProps({
+const props = defineProps({
+  msg: {
+    type: String,
+    required: true,
+  }
+});
+
+const textInput2 = ref('');
+const textInput3 = ref('');
+
+</script>
+
 <template>
   <div class="hello">
+    <h1>{{ msg }}</h1>
     <!-- 2. 本文 -->
     <!-- レイアウトのためのsection、containerコンポーネント -->
     <section class="section">
@@ -13,31 +33,18 @@
             <input type="text" class="input">
           </div>
         </div>
-        <!-- <div class="columns">
+        <div class="columns">
           <div class="column is-half">
             入力欄2
           </div>
           <div class="column">
-            <input type="text" class="input" v-model="textInput" />
+            <input type="text" class="input" v-model="textInput2" />
           </div>
-        </div> -->
+        </div>
+        <ColumnInput v-model="textInput3" />
+        <CountButton />
       </div>
-      <CountButton />
       <!-- <button-counter></button-counter> -->
     </section>
   </div>
 </template>
-
-<script>
-import CountButton from '../atoms/CountButton.vue'
-
-export default {
-  name: 'HogeFuga',
-  components: {
-    CountButton
-  },
-  props: {
-    msg: String
-  }
-}
-</script>
