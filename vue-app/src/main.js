2427c59d2d1cb6aad091b19bd3f4b07cf3f48f5d
sakuteya <dearka.vermilion@gmail.com>
2025-09-27 21:03:03 +0900

    算出プロパティのガワ追加

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import HomeView from './pages/HomeView.vue'
 import HogeFuga from './pages/HogeFuga.vue'
 import TemplateSyntax from './pages/TemplateSyntax.vue'
+import ComputedView from './pages/ComputedView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/hoge-fuga', component: HogeFuga, props: { msg: 'from router' } },
-  { path: '/template-syntax', component: TemplateSyntax }
+  { path: '/template-syntax', component: TemplateSyntax },
+  { path: '/computed', component: ComputedView },
 ]
 
 const router = createRouter({
